d47f469e4ffc05871a10842a4fb45b8dbbe2a3de
jrector <jrector@mindstreaminteractive.com>
2015-10-30 11:22:02 -0400

    Initial Commit.

---
+++ gulp/templates.js
@@ -0,0 +1,40 @@
+var $ = require('./common.js');
+var config = require('./config.js');
+
+//Jade
+// var jade = require('gulp-jade');
+// var jadeInheritance = require('gulp-jade-inheritance');
+// var filter = require('gulp-filter');
+
+// var jadeSrc = './' + config.src + 'views/';
+
+// $.gulp.task('views', function() {
+//     return $.gulp.src(jadeSrc + '**/*.jade')
+//         .pipe($.plumber({ errorHandler: $.notify.onError('<%= error.message %>') }))
+//         .pipe(jadeInheritance({ basedir: jadeSrc }))
+//         .pipe(jade({
+//             basedir: jadeSrc,
+//             pretty: true,
+//             locals: { production: config.prod }
+//         }))
+//         .pipe(filter(function(file) {
+//             var exclude;
+//             if (config.outputJadeIncludes) exclude = new RegExp('templates|mixins', 'g');
+//             else exclude = new RegExp('templates|mixins|includes', 'g');
+//
+//             return !exclude.test(file.path);
+//         }))
+//         .pipe($.gulp.dest(config.dest));
+// });
+
+//HTML includes
+var fileinclude = require('gulp-file-include');
+
+$.gulp.task('fileinclude', function() {
+    $.gulp.src([config.src + 'views/*.html'])
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
+    .pipe($.gulp.dest(config.dest));
+});
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var requireDir = require('require-dir');
+var config = require('./gulp/config.js');
+
+requireDir('./gulp/', { recurse: true });
+
+gulp.task('clean', require('del').bind(null, [config.dest + '**/*', '!.*']));
+
+gulp.task('default', ['clean'], function() {
+    // gulp.start('styles', 'images', 'scripts', 'views'); // Jade
+    gulp.start('styles', 'images', 'scripts', 'fileinclude'); // HTML
+});
+
+gulp.task('prod', function() {
+    config.prod = true;
+    gulp.start('default');
+});
+
+gulp.task('watch', function() {
+    config.watch = true;
+    gulp.start('scripts', 'watching');
+});
